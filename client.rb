c932862534c2ed873f509f661e05ff241b5bd114
opsb <oliver@opsb.co.uk>
2011-08-04 18:20:46 +0100

    Allow credentials to be loaded from ENV['CREDENTIALS']

--- client.rb
+++ client.rb
@@ -4,7 +4,7 @@ require './xero.rb'
 require './account.rb'
 require 'chronic'
 
-credentials = YAML.load(File.read('credentials.yaml'))
+credentials = ENV['CREDENTIALS'] || YAML.load(File.read('credentials.yaml'))
 
 natwest = Natwest.new credentials[:natwest]
 natwest.login!
@@ -13,7 +13,7 @@ xero = Xero.new credentials[:xero]
 xero.login!
 
 credentials[:accounts].each do |account|
-  file_path = natwest.download_statement( account.id, Chronic.parse("16 weeks ago"), Time.now )
+  file_path = natwest.download_statement( account.id, Chronic.parse(ENV['SYNC_FROM'] || "16 weeks ago"), Time.now )
   xero.upload_statement! account, file_path
 end
 
